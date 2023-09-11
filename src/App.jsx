b3276119e10169c7beacee13ec4182be496c421a
hftamayo <hftamayo@gmail.com>
2023-09-11 15:28:02 -0600

    adding createUSer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Users from "./Users";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Users from "./Users";
+import CreateUser from "./CreateUser";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Users />}></Route>
+      <Route path="/create" element={<AddUser />}></Route>
     </Routes>
     </BrowserRouter>
   );
