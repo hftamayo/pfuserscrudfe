4c3e1734272ba60de7cf5f9fbfb3fac918c7af10
hftamayo <hftamayo@gmail.com>
2023-09-12 13:06:34 -0600

    moving fetchData method to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Users from "./Users";
 import CreateUser from "./CreateUser";
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8004");
+        dispatch(getUser(response.data));
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <BrowserRouter>
     <Routes>
--- src/Users.jsx
+++ src/Users.jsx
@@ -1,6 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect } from "react";
-import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getUser } from "./redux/userSlice";
@@ -11,18 +9,6 @@ function Users() {
   const users = useSelector((state) => state.users.users);
   console.log(useSelector((state) => state.users.users));
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:8004");
-        dispatch(getUser(response.data));
-        console.log(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
   return (
     <div className="d-flex vh-100 vw-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-5">
