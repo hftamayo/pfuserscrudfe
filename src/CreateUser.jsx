a948a2e51cea95b2766d713937552a483db6cf53
hftamayo <hftamayo@gmail.com>
2023-09-13 11:19:19 -0600

    adjusting endpoint name

--- src/CreateUser.jsx
+++ src/CreateUser.jsx
@@ -8,7 +8,7 @@ function CreateUser() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post("http://localhost:8004/create", {name, email, age})
+    axios.post("http://localhost:8004/createuser", {name, email, age})
     .then(res => console.log(res))
     .catch(err => console.log(err))
   };
