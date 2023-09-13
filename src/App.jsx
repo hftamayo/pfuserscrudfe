8804907e8591317d9c81499d15700a1ba3586df0
hftamayo <hftamayo@gmail.com>
2023-09-13 10:58:52 -0600

    adjusting control for data displaying

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Users from "./Users";
 import CreateUser from "./CreateUser";
+import { getUser } from "./redux/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
+    
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:8004");
+        const response = await axios.get("http://localhost:8004/getusers");
         dispatch(getUser(response.data));
         console.log(response.data);
       } catch (error) {
--- src/Users.jsx
+++ src/Users.jsx
@@ -1,13 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { getUser } from "./redux/userSlice";
 
 
 function Users() {
-  const dispatch = useDispatch();
   const users = useSelector((state) => state.users.users);
-  console.log(useSelector((state) => state.users.users));
 
   return (
     <div className="d-flex vh-100 vw-100 bg-primary justify-content-center align-items-center">
