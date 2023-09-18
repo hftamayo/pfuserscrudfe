d461f7ca96062c67bb926ec355cd1092831802fd
hftamayo <hftamayo@gmail.com>
2023-09-18 15:19:52 -0600

    adding dispatch and axios

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,15 +1,28 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-
+import { deleteUser } from "./redux/userSlice";
+import axios from "axios";
 
 function Users() {
   const users = useSelector((state) => state.users.users);
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    axios
+      .delete("http://localhost:8004/deleteuser/" + id)
+      .then((res) => {
+        dispatch(deleteUser({id}));
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="d-flex vh-100 vw-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-5">
-        <Link to="/create" className="btn btn-success btn-sm">Add +</Link>
+        <Link to="/create" className="btn btn-success btn-sm">
+          Add +
+        </Link>
         <table className="table">
           <thead>
             <tr>
@@ -21,15 +34,27 @@ function Users() {
           </thead>
           <tbody>
             {users.map((user) => {
-              return <tr>
-                <td>{user.firstname + " " + user.lastname}</td>
-                <td>{user.email}</td>
-                <td>{user.age}</td>
-                <td>
-                  <Link to={`/edit/${user.id}`} className="btn btn-sm btn-success me-2">Update</Link>
-                  <Link to={`/delete/${user.id}`} className="btn btn-sm btn-danger">Delete</Link>
-                </td>
-              </tr>;
+              return (
+                <tr>
+                  <td>{user.firstname + " " + user.lastname}</td>
+                  <td>{user.email}</td>
+                  <td>{user.age}</td>
+                  <td>
+                    <Link
+                      to={`/edit/${user.id}`}
+                      className="btn btn-sm btn-success me-2"
+                    >
+                      Update
+                    </Link>
+                    <button
+                      onClick={() => handleDelete(user.id)}
+                      className="btn btn-sm btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
             })}
           </tbody>
         </table>
